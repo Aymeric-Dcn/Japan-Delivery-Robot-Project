ddac572927426184253d947244657d97eb98278d
Aymeric-Dcn <aymeric.duchene@outlook.fr>
2026-05-15 23:21:02 +0900

    Working Ultrasonic sensor demo code

--- Japan-Locker-Project.ino
+++ Japan-Locker-Project.ino
@@ -1,8 +1,42 @@
+#define TRIG_PIN 39
+#define ECHO_PIN 40
+#define LED_PIN 6
+
+long duration;
+float distance;
+
 void setup() {
-  Serial.begin(115200);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  pinMode(LED_PIN, OUTPUT);
+
+  digitalWrite(TRIG_PIN, LOW);
 }
 
 void loop() {
-  Serial.println("Je suis en vie");
-  delay(1000);
+
+  // Pulse trigger
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Lecture echo
+  duration = pulseIn(ECHO_PIN, HIGH, 30000);
+
+  // Conversion distance cm
+  distance = duration * 0.017;
+
+  // Objet détecté à moins de 20 cm
+  if (distance > 0 && distance < 20) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  delay(50);
 }
