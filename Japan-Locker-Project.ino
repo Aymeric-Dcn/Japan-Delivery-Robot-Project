c6aea4e61e07c842217969c4345534983c9c28f7
Aymeric-Dcn <aymeric.duchene@outlook.fr>
2026-05-18 11:37:54 +0900

    Working ultrasonic sensor with buffer to store the distances

--- Japan-Locker-Project.ino
+++ Japan-Locker-Project.ino
@@ -1,6 +1,8 @@
 #define TRIG_PIN 39
 #define ECHO_PIN 40
 #define LED_PIN 6
+#define DELAY   500 // This delay represents the sampling of the distance, change if you want sampling faster of slower
+
 
 long duration;
 float distance;
@@ -12,10 +14,13 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
 
   digitalWrite(TRIG_PIN, LOW);
+  Serial.begin(115200);
 }
 
 void loop() {
 
+  // Internal variables
+  float distance_Buffer;
   // Pulse trigger
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
@@ -25,18 +30,27 @@ void loop() {
 
   digitalWrite(TRIG_PIN, LOW);
 
-  // Lecture echo
+  // Reading echo
   duration = pulseIn(ECHO_PIN, HIGH, 30000);
 
-  // Conversion distance cm
+  // Conversion in cm
   distance = duration * 0.017;
+ 
+  Serial.println("Distance = ");
+  Serial.println(distance);
+  Serial.println("cm");
+  distance_Buffer = distance;
+  delay(DELAY);
 
-  // Objet détecté à moins de 20 cm
-  if (distance > 0 && distance < 20) {
+
+  // Object detected (less than 1m) One can use it as test or debug purposes
+  if (distance > 0 && distance < 100) {
+    Serial.println("Obstacle detected !");
     digitalWrite(LED_PIN, HIGH);
   } else {
     digitalWrite(LED_PIN, LOW);
   }
+
 
   delay(50);
 }
