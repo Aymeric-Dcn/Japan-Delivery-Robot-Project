e90a015d9899c1dd7c2ba31db04d4ec080e24c61
Aymeric-Dcn <aymeric.duchene@outlook.fr>
2026-06-21 18:15:14 +0900

    Web interface with button and alerts

---
+++ RobotWeb/frontend/script.js
@@ -0,0 +1,9 @@
+function hello() {
+    alert("Hello!");
+}
+
+function sendDestination() {
+    let destination = document.getElementById("destination").value;
+
+    alert("Destination: " + destination);
+}
