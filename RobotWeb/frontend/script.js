c0fc6c31e9e969b8756b559e99d533e3a12959f0
Aymeric-Dcn <aymeric.duchene@outlook.fr>
2026-06-23 13:37:33 +0900

    Login page and destination sending page

--- RobotWeb/frontend/script.js
+++ RobotWeb/frontend/script.js
@@ -7,3 +7,44 @@ function sendDestination() {
 
     alert("Destination: " + destination);
 }
+
+function login(){
+
+    const username =
+        document.getElementById("username").value;
+
+    const password =
+        document.getElementById("password").value;
+
+    if(username==="admin" && password==="robot"){
+
+        window.location.href = "index.html";
+        document.getElementById("message").innerHTML =
+            "Connexion réussie";
+
+    }
+
+    else{
+
+        document.getElementById("message").innerHTML =
+            "Identifiants incorrects";
+
+    }
+
+}
+
+const socket = new WebSocket("ws://localhost:8080");
+
+socket.onopen = () => {
+    console.log("Connecté");
+};
+
+socket.onmessage = (event) => {
+    console.log("Robot :", event.data);
+};
+
+socket.onclose = () => {
+    console.log("Déconnecté");
+};
+
+
